a0fe198e9882a5744491ea7a8041e61aa359ce8a
Nemeth Gergely <gergo.nemeth98@gmail.com>
2021-03-03 10:37:50 +0100

    Add bird_detection.tflite and add permissions

--- BirdDetection_First/app/src/main/java/hu/geribruu/birddetection_first/MainActivity.kt
+++ BirdDetection_First/app/src/main/java/hu/geribruu/birddetection_first/MainActivity.kt
@@ -1,11 +1,147 @@
 package hu.geribruu.birddetection_first
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.mlkit.common.model.LocalModel
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.objects.ObjectDetection
+import com.google.mlkit.vision.objects.ObjectDetector
+import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions
+import hu.geribruu.birddetection_first.databinding.ActivityMainBinding
+import hu.geribruu.birddetection_first.utils.Draw
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private lateinit var objectDetector : ObjectDetector
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            bindPreview(cameraProvider = cameraProvider)
+
+        }, ContextCompat.getMainExecutor(this))
+
+        val localModel = LocalModel.Builder()
+            .setAbsoluteFilePath("bird_detection.tflite")
+            .build()
+
+        val customObjectDetectorOption = CustomObjectDetectorOptions.Builder(localModel)
+            .setDetectorMode(CustomObjectDetectorOptions.STREAM_MODE)
+            .enableClassification()
+            .setClassificationConfidenceThreshold(0.5f)
+            .setMaxPerObjectLabelCount(3)
+            .build()
+
+        objectDetector = ObjectDetection.getClient(customObjectDetectorOption)
+    }
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+
+        val preview = Preview.Builder().build()
+
+        val cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        preview.setSurfaceProvider(binding.previewView.surfaceProvider)
+
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setTargetResolution(Size(1280, 720))
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), { imageProxy ->
+            val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+
+            val image = imageProxy.image
+
+            if(image != null) {
+                val processImage = InputImage.fromMediaImage(image, rotationDegrees)
+
+                objectDetector.process(processImage)
+                    .addOnSuccessListener { objects ->
+
+                        for (i in objects) {
+
+                            if(binding.parentLayout.childCount > 1)  {
+                                binding.parentLayout.removeViewAt(1)
+                            }
+
+                            val element = Draw(context = this, rect = i.boundingBox, text = i.labels.firstOrNull()?.text ?: "Undefined")
+                            binding.parentLayout.addView(element,1)
+                        }
+
+
+                        imageProxy.close()
+                    }.addOnFailureListener {
+                        Log.v("MainActivity", "Error - ${it.message}")
+                        imageProxy.close()
+                    }
+            }
+        })
+
+        cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, imageAnalysis, preview)
     }
 }
